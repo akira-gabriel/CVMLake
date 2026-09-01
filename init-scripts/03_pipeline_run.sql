c727fac46d28024f2d279f51fb0a229cb5f5fbea
Gabriel Akira <gabriel.akira2198@gmail.com>
2026-09-01 00:33:21 -0300

    Create pipeline_run table in control schema

---
+++ init-scripts/03_pipeline_run.sql
@@ -0,0 +1,8 @@
+CREATE TABLE control.pipeline_run (
+    id UUID PRIMARY KEY,
+    dataset TEXT NOT NULL,
+    started_at TIMESTAMPTZ  NOT NULL,
+    finished_at TIMESTAMPTZ NULL,
+    CONSTRAINT chk_pipeline_temporal CHECK (finished_at >= started_at),
+    status TEXT CONSTRAINT chk_pipeline_run_status CHECK (status IN ('running','success','partial_success','fail')) DEFAULT 'running' NOT NULL
+)
