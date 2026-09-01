19aeecbaec28ab75fa332e8c1e06f2989a39697c
Gabriel Akira <gabriel.akira2198@gmail.com>
2026-09-01 15:44:27 -0300

    sql tables updated

--- init-scripts/01_pipeline_run.sql
+++ init-scripts/01_pipeline_run.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS control;
+
 CREATE TABLE control.pipeline_run (
     id UUID PRIMARY KEY,
     dataset TEXT NOT NULL,
@@ -5,4 +7,4 @@ CREATE TABLE control.pipeline_run (
     finished_at TIMESTAMPTZ NULL,
     CONSTRAINT chk_pipeline_temporal CHECK (finished_at >= started_at),
     status TEXT CONSTRAINT chk_pipeline_run_status CHECK (status IN ('running','success','partial_success','fail')) DEFAULT 'running' NOT NULL
-)
+);
--- init-scripts/02_ingestion_log.sql
+++ init-scripts/02_ingestion_log.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS control;
-
 CREATE TABLE control.ingestion_log (
     id SERIAL PRIMARY KEY,
+    run_id UUID NOT NULL CONSTRAINT fk_ingestion_log REFERENCES control.pipeline_run(id),
     event_timestamp TIMESTAMPTZ NOT NULL,
     dataset VARCHAR(30) NOT NULL,
     status TEXT CHECK (status IN ('success', 'fail')) NOT NULL,
     rows_affected INTEGER NOT NULL DEFAULT 0,
     error_message TEXT
 );
+CREATE INDEX idx_ingestion_log ON control.ingestion_log(run_id);
