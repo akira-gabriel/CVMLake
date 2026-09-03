bce90a4663b8d0bdd32b579c47ed044be220ba9c
Gabriel Akira <gabriel.akira2198@gmail.com>
2026-09-03 00:12:14 -0300

    fix

--- init-scripts/02_ingestion_log.sql
+++ init-scripts/02_ingestion_log.sql
@@ -1,10 +1,10 @@
 CREATE TABLE control.ingestion_log (
     id SERIAL PRIMARY KEY,
-    run_id UUID NOT NULL CONSTRAINT fk_ingestion_log REFERENCES control.pipeline_run(id),
+    run_id UUID NOT NULL CONSTRAINT fk_ingestion_log_run_id REFERENCES control.pipeline_run(id) ON DELETE RESTRICT,
     event_timestamp TIMESTAMPTZ NOT NULL,
     dataset VARCHAR(30) NOT NULL,
     status TEXT CHECK (status IN ('success', 'fail')) NOT NULL,
     rows_affected INTEGER NOT NULL DEFAULT 0,
     error_message TEXT
 );
-CREATE INDEX idx_ingestion_log ON control.ingestion_log(run_id);
+CREATE INDEX idx_ingestion_log_run_id ON control.ingestion_log(run_id);
